5b22f2ff03f4e28636e73e1bbfa741c0cbc49893
habo <jhb990438@gmail.com>
2025-01-07 15:36:17 +0900

    Tdd Test Init

---
+++ src/test/java/com/example/TddTest/TddTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.TddTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
---
+++ src/test/java/com/example/TddTest/WineControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.TddTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WineControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldCreateWine() throws Exception {
+        String wineJson = """
+                {
+                    "name" : "Minsu",
+                    "country" : "korea",
+                    "variety" : "white",
+                    "price" : 20.0
+                }
+                """;
+
+        mockMvc.perform(post("/wines")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(wineJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.name").value("Minsu"))
+                .andExpect(jsonPath("$.country").value("korea"))
+                .andExpect(jsonPath("$.variety").value("white"))
+                .andExpect(jsonPath("$.price").value(20.0));
+    }
+}
